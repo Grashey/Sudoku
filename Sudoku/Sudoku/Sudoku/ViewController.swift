cb79c1ea6358b8c63de1855c6a3854709a50414e
Grashey <grashey@mail.ru>
2022-05-16 17:23:20 +0300

    project init

---
+++ Sudoku/Sudoku/Sudoku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by Aleksandr Fetisov on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
