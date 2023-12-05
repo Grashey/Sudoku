f9c14303a7a285080a442aae5173bf8d8000d6a9
Grashey <grashey@mail.ru>
2023-12-05 11:35:29 +0300

    Добавил проверку на стартовое НЕзаполнение области

--- Sudoku/Sudoku/Sudoku/GameLogic/InitialStateLoader.swift
+++ Sudoku/Sudoku/Sudoku/GameLogic/InitialStateLoader.swift
@@ -10,25 +10,28 @@ import Foundation
 class InitialStateLoader {
     
     func start(with mode: GameMode) -> [Position] {
+        areas = makeAreas()
+        
         switch mode {
         case .easy:
-            maxValues = 38
+            startOpenedValues = 38
             maxValuesInRow = 8
         case .normal:
-            maxValues = 30
+            startOpenedValues = 30
             maxValuesInRow = 7
         }
         return makeInitialPositions()
     }
     
     private var state: [Position] = []
-    private var maxValues: Int?
+    private var startOpenedValues: Int?
     private var maxValuesInRow: Int?
     private let maxBoardIndex = 8
+    private var areas: [[Position]]?
     
     private func makeInitialPositions() -> [Position] {
-        guard let maxValues = maxValues else { return [] }
-        while state.count < maxValues {
+        guard let startOpenedValues = startOpenedValues else { return [] }
+        while state.count < startOpenedValues {
             if let position = makePosition() {
                 state.append(position)
             }
@@ -45,12 +48,42 @@ class InitialStateLoader {
         let valuesInRow = state.map({ $0.row == row }).filter({$0 == true}).count
         let valuesInColumn = state.map({ $0.column == column }).filter({$0 == true}).count
         
+        guard let areas = areas, let area = areas.first(where: { $0.contains(where: { $0 == position })}) else { return nil }
+        var valuesinArea: Int = .zero
+        for position in area {
+            if state.contains(where: { $0 == position }) {
+                valuesinArea += 1
+            }
+        }
+        
         if !state.contains(where: { position == $0 }) {
-            if valuesInRow < maxValuesInRow, valuesInColumn < maxValuesInRow {
+            if valuesInRow < maxValuesInRow, valuesInColumn < maxValuesInRow, valuesinArea < maxValuesInRow {
                 return position
             }
         }
         return nil
+    }
+    
+    private func makeAreas() -> [[Position]] {
+        var areas: [[Position]] = []
+        let firstIndexes = [0,1,2]
+        let secondIndexes = [3,4,5]
+        let thirdIndexes = [6,7,8]
+        let indexes = [firstIndexes, secondIndexes, thirdIndexes]
+        
+        for areaInRow in indexes {
+            for areaInColumn in indexes {
+                var areaPositions: [Position] = []
+                for row in areaInRow {
+                    for column in areaInColumn {
+                        let position = Position(row: row, column: column)
+                        areaPositions.append(position)
+                    }
+                }
+                areas.append(areaPositions)
+            }
+        }
+        return areas
     }
     
 }
